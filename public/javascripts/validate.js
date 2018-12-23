e45f7f43d4e976580e9114d4dedc2f4f9c5aed12
Koustav Samaddar <koustav201197@gmail.com>
2018-12-23 21:16:33 +0400

    Added spinner on search while results load

--- public/javascripts/validate.js
+++ public/javascripts/validate.js
@@ -1,5 +1,14 @@
 
-const validateSearch = function () {
+function validateSearch() {
   const isEmpty = (/^\s*$/).test(document.getElementsByClassName('searchfield')[0].value);
+  if (!isEmpty) {
+    document.getElementById('loader').removeAttribute('hidden');
+  };
   return !isEmpty;
 };
+
+function onSearchPageLoad() {
+  document.getElementById('loader').setAttribute('hidden', '');
+}
+
+onSearchPageLoad();
